cdde75018b580a706626720055f09a0e88b5d5af
Telman Babayev <babayevtolman@gmail.com>
2022-11-05 19:30:54 +0400

    Complete section 5

--- section-5/app.js
+++ section-5/app.js
@@ -1,6 +1,20 @@
-const http = require("http");
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
+const app = express();
 
-const server = http.createServer();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-server.listen(3000);
+app.use((req, res, next) => {
+  res
+    .status(404)
+    .sendFile(path.join(__dirname, "views", "page-not-found.html"));
+});
+
+app.listen(3000);
