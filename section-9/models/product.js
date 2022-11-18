6c03aea1378cd822cff1e0b664920a90b6218890
Telman Babayev <babayevtolman@gmail.com>
2022-11-18 21:50:02 +0400

    Implement section 9

---
+++ section-9/models/product.js
@@ -0,0 +1,81 @@
+const fs = require("fs");
+const path = require("path");
+
+const Cart = require("./cart");
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    }
+
+    cb(JSON.parse(fileContent));
+  });
+};
+
+class Product {
+  constructor(id, title, imageURL, description, price) {
+    this.title = title;
+    this.imageURL = imageURL;
+    this.description = description;
+    this.price = price;
+    this.id = id;
+  }
+
+  save() {
+    getProductsFromFile((products) => {
+      if (this.id) {
+        const existProductIndex = products.findIndex(
+          (prod) => prod.id === this.id
+        );
+        const updatedProducts = [...products];
+        updatedProducts[existProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+      }
+    });
+  }
+
+  static fetchAll(callback) {
+    getProductsFromFile(callback);
+  }
+
+  static findById(id, callback) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      callback(product);
+    });
+  }
+
+  static deleteProduct(id) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      const updatedProducts = products.filter((p) => p.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price);
+        }
+        console.log(err);
+      });
+    });
+  }
+}
+
+module.exports = Product;
